f50fd2148ff70fe88af2cb4e23f6dc0dcf4d0470
Gordey.Evstafyev <wowspec2010@mail.ru>
2024-11-21 20:39:56 +0400

    added methods for class house

--- module5_1.py
+++ module5_1.py
@@ -1,18 +1,34 @@
 class House:
-    def __init__(self, name, floors_count):
+    def __init__(self, name, number_of_floors):
         self.name = name
-        self.floors_count = floors_count
+        self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-        if self.floors_count < new_floor:
+        if self.number_of_floors < new_floor:
             print("Такого этажа не существует")
         else:
             for i in range(new_floor):
                 print(i + 1)
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 
 h1.go_to(5)
 h2.go_to(10)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
